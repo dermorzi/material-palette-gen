28a93aa43eba68ae876f9dedf15a2a47b95dc157
Rene Pohland <rene@webograph.de>
2023-07-30 20:22:16 +0200

    example files cleanup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <span class="title">Material Color Palette Generator</span>
+  </header>
+  <RouterView />
+</template>
